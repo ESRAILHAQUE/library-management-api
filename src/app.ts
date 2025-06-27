e8a1afa4f584357000f16ebc82505065dd1a4b23
ESRAILHAQUE <esrailbblhs@gmail.com>
2025-06-28 01:34:01 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import bookRoutes from './routes/book.route';
+import borrowRoutes from './routes/borrow.route';
+dotenv.config();
+
+const app:Application = express();
+
+
+
+// middleware
+app.use(express.json());
+// Book routes
+app.use('/api',bookRoutes);
+
+// Borrow routes
+app.use("/api", borrowRoutes);
+
+app.get('/', (req:Request, res:Response) => {
+  res.send({
+    message: 'Welcome to the Library Management API',
+    status: 'success',
+    data: {
+      name: 'Library Management API',
+      version: '1.0.0',
+      description: 'An API for managing library operations including books, authors, and members.',
+    },
+  });
+});
+
+export default app;
---
+++ src/models/book.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IBook } from "../interfaces/book.interface";
+
+export interface IBookDocument extends IBook, Document {
+  updateAvailability: () => Promise<IBookDocument>;
+}
+
+const bookSchema = new Schema<IBookDocument>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+      required: true,
+    },
+    isbn: { type: String, required: true, unique: true },
+    description: { type: String, default: "" },
+    copies: { type: Number, required: true, min: 1 },
+    available: { type: Boolean, default: true },
+
+  },
+  { timestamps: true,versionKey: false } 
+
+);
+
+bookSchema.methods.updateAvailability = async function (): Promise<IBookDocument> {
+  this.available = this.copies > 0;
+  return this.save();
+};
+
+export const  Book = mongoose.model<IBookDocument>("Book", bookSchema);
